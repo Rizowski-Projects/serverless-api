ba8ba833b277d1533c68ee300bdc59285b953aea
Colten Rouska <Colten.Rouska@extendhealth.com>
2017-02-03 15:16:34 -0700

    Enforcing binary data

--- functions/instagram.js
+++ functions/instagram.js
@@ -9,13 +9,10 @@ module.exports = {
         accept: 'application/json'
       }
     }, function(err, res, body){
-      console.log(res.statusCode, err);
       if(!err && res.statusCode === 200){
         var image = _.get(JSON.parse(res.body), 'data[0].images.standard_resolution.url');
-        console.log(typeof image, image);
-        console.log('YEA THERE IS');
-        return r({ url: image }, function(err, res, body){
-          callback(null, res.body);
+        return r({ url: image, encoding: null }, function(err, res, body){
+          callback(null, res.body );
         });
       }
       callback(err);
