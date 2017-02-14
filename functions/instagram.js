53fc7dceac78205d38c0ece6f919dece57cf235d
Colten Rouska <Colten.Rouska@extendhealth.com>
2017-02-14 13:05:08 -0700

    Changing to node-fetch

--- functions/instagram.js
+++ functions/instagram.js
@@ -1,21 +1,19 @@
-var r = require('request');
+var fetch = require('node-fetch');
 var _ = require('lodash');
 
 module.exports = {
   getLatestPhoto: function getLatestPhoto(event, context, callback){
-    r({
-      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + process.env.IG_TOKEN,
-      headers: {
-        accept: 'application/json'
-      }
-    }, function(err, res, body){
-      if(!err && res.statusCode === 200){
-        var image = _.get(JSON.parse(res.body), 'data[0].images.standard_resolution.url');
-        return r({ url: image, encoding: null }, function(err, res, body){
-          callback(null, res.body.toString('ascii') );
-        });
-      }
-      callback(err);
-    });
+    fetch('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + process.env.IG_TOKEN, { headers: { 'Accept': 'application/json' } })
+      .then((res) => res.json())
+      .then((json) => {
+        var thing = _.get(json, 'data[0].images.standard_resolution.url');
+        console.log(thing);
+        return thing;
+      })
+      .then((url) => fetch(url, { headers : { 'Content-Type': 'image/jpeg' } }))
+      .then((res) =>{
+        console.log(Object.keys(res));
+        callback(null, res.buffer() );
+      });
   }
 }
